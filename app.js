a11e9b6dba219a8561bb9f6fcb585da9eb50e235
rmcelroy1990 <mcelroy.raeme@gmail.com>
2025-02-28 01:03:54 -0800

    completed lab

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const buttons = document.querySelectorAll('.button');
-const calculator = document.querySelector('#calculator');
-
 /*-------------------------------- Variables --------------------------------*/
 
 let firstValue = null;
@@ -10,52 +7,44 @@ let secondValue = null;
 let operation = null;
 
 /*------------------------ Cached Element References ------------------------*/
-let totalDisplay = document.querySelector('.display');
-  totalDisplay.innerText = 0;
 
-let equalsButton = document.querySelector('#equals');
-let clearButton = document.querySelector('#clear');
+let numbers = document.querySelectorAll('.number');
+let operatorButtons = document.querySelectorAll('.operator');
 
-let addButton = document.querySelector('#add');
+let addButton = document.querySelector('.add');
 let subtractButton = document.querySelector('#subtract');
 let multiplyButton = document.querySelector('#multiply');
 let divideButton = document.querySelector('#divide');
 
-let numbers = document.querySelectorAll('.number');
-let operationButtons = document.querySelectorAll('.operation');
-/*----------------------------- Event Listeners -----------------------------*/
+let equalsButton = document.querySelector('#equals');
+let clearButton = document.querySelector('#clear');
+
+let totalDisplay = document.querySelector('.display');
+totalDisplay.innerText = 0;
+
+/*-------------------------------- Functions --------------------------------*/
 
 equalsButton.addEventListener('click', handleEqualBtnClick);
 clearButton.addEventListener('click', clear);
 
-//button.addEventListener('click', (event) => {}
-  buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-      // This log is for testing purposes to verify we're getting the correct value
-      console.log(calculator.target.innerText);
-      // Future logic to capture the button's value would go here...
-    });
+numbers.forEach((number) => {
+  number.addEventListener('click', (event) => {
+    totalDisplay.innerText = Number(
+      totalDisplay.innerText + event.target.innerText
+    );
   });
+});
 
-  numbers.forEach((number) => {
-    number.addEventListener('click', (event) => {
-      totalDisplay.innerText = Number(
-        totalDisplay.innerText + event.target.innerText
-      );
-    });
-  });
-
-  operationButtons.forEach((operation) => {
-    operation.addEventListener('click', handleOperatorClick);
-  });
-/*-------------------------------- Functions --------------------------------*/
-
-const subtract = () => {
-  return firstValue - secondValue;
-};
+operatorButtons.forEach((operator) => {
+  operator.addEventListener('click', handleOperatorClick);
+});
 
 const add = () => {
   return firstValue + secondValue;
+};
+
+const subtract = () => {
+  return firstValue - secondValue;
 };
 
 const multiply = () => {
@@ -66,27 +55,10 @@ const divide = () => {
   return firstValue / secondValue;
 };
 
-  calculator.addEventListener('click'), (addNumbers) => {
-    // This log is for testing purposes to verify we're getting the correct value
-    // You have to click a button to see this log
-    console.log(event.target.innerText);
-
-    // Example
-  }
-  if (event.target.classList.contains('number')) {
-    // Do something with a number
-  }
-
-  // Example
-  if (event.target.innerText === '*') {
-    // Do something with this operator
-  }
-
-  const renderCalculation = () => {
-    let total;
-    if (opertion === 'add') {
-      total = add();
-    }
+const renderCalculation = () => {
+  let total;
+  if (operation === 'add') {
+    total = add();
   }
   if (operation === 'subtract') {
     total = subtract();
@@ -97,22 +69,27 @@ const divide = () => {
   if (operation === 'divide') {
     total = divide();
   }
+  totalDisplay.innerHTML = total;
+};
 
-  const clear = () => {
-    totalDisplay.innerText = 0;
-    firstValue = null;
-    secondValue = null;
-    operation = null;
-  };
-
- const handleEqualBtnClick = () => {
+const handleEqualBtnClick = () => {
   secondValue = parseInt(totalDisplay.innerText);
   renderCalculation();
- };
+};
 
- const handleOperatorClick = () => {
-  frstValue = parseInt(totalDisplay.innerText);
-  operation = event.target.dispatchEvent;
+const handleOperatorClick = (event) => {
+  firstValue = parseInt(totalDisplay.innerText);
+  operation = event.target.id;
   totalDisplay.innerText = 0;
- };
+};
+
+const clear = () => {
+  totalDisplay.innerText = 0;
+  firstValue = null;
+  secondValue = null;
+  operation = null;
+};
+
+/*----------------------------- Event Listeners -----------------------------*/
+
 
